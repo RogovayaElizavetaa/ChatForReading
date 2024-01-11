55503c569bab9b457c4195f9ca813446f11eba95
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-01-11 15:34:21 +0200

    added start pages

---
+++ frontend/src/router/router.js
@@ -0,0 +1,10 @@
+import Start from '@/pages/Start'
+import { createRouter } from 'vue-router'
+const routes = [
+    {
+        path: '/',
+        component:Start,
+    }
+]
+
+const router = createRouter
